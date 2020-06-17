f556a144bba4bf8bcc3c0befbfb87064ef607f06
Juan Parra <juandkpa@gmail.com>
2020-06-17 18:31:08 -0500

    sync events

--- packages/query/index.js
+++ packages/query/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,13 +10,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const { type, data} = req.body;
-
+const handleEvent = (type, data) => {
     switch(type) {
         case 'PostCreated': {
                 const { id, title } = data;
@@ -36,9 +31,27 @@ app.post('/events', (req, res) => {
             comment.content = content;
         }
     }
+}
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data} = req.body;
+
+    handleEvent(type, data);
+
     res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Listening on 4002');
+
+    const res = await axios.get('http://localhost:4005/events');
+
+    for (let event of res.data) {
+        console.log('Processing event:', event.type);
+        handleEvent(event.type, event.data);
+    }
 })
