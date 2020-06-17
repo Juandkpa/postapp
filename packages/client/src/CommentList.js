1f7cab89b5691cb87c7a3e3c7c6cee04cc1fa808
Juan Parra <juandkpa@gmail.com>
2020-06-17 15:39:56 -0500

    using query microservice

--- packages/client/src/CommentList.js
+++ packages/client/src/CommentList.js
@@ -1,21 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-const fetchData = async(postId, setComments) => {
-    const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-    setComments(res.data);
-};
 
-export default ({ postId }) => {
-    const [comments, setCommets] = useState([]);
 
-    useEffect(() => {
-        fetchData(postId, setCommets);
-    }, [postId]);
-
+export default ({ comments }) => {
     const renderedComments = comments.map(comment => {
         return <li key={comment.id}>{comment.content}</li>
-    })
+    });
 
     return (
         <ul>
